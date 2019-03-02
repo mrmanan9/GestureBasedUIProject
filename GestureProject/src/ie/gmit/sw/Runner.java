7a3bc35da0e615336963e9d9a15dd5ad839e4216
Nakster <nakster58@gmail.com>
2019-03-02 21:20:22 +0000

    added the runner file

---
+++ GestureProject/src/ie/gmit/sw/Runner.java
@@ -0,0 +1,20 @@
+package ie.gmit.sw;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class Runner extends JFrame implements ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
