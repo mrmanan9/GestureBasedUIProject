7cb618bf9d991186184334eb175a59fcc171d8bc
Nakster <nakster58@gmail.com>
2019-03-04 15:15:23 +0000

    test

--- GestureProject/src/ie/gmit/sw/Runner.java
+++ GestureProject/src/ie/gmit/sw/Runner.java
@@ -1,14 +1,20 @@
 package ie.gmit.sw;
 
+<<<<<<< HEAD
 import java.awt.FontFormatException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+=======
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+>>>>>>> 6b0c586... added the runner file
 
 import javax.swing.JFrame;
 
 public class Runner extends JFrame implements ActionListener{
 
+<<<<<<< HEAD
 	private static final long serialVersionUID = 1L;
 	
 	public Runner() {
@@ -21,14 +27,23 @@ public class Runner extends JFrame implements ActionListener{
         setResizable(false);
         setVisible(true);
 	}
+=======
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+>>>>>>> 6b0c586... added the runner file
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+<<<<<<< HEAD
 	
 	public static void main(String[]args) throws IOException, FontFormatException{
         new Runner();
     }
+=======
+>>>>>>> 6b0c586... added the runner file
 }
