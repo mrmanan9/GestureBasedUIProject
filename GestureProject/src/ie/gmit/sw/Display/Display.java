ef8087cff3b90e77ccff49bca4183e03131df608
Nakster <nakster58@gmail.com>
2019-03-04 14:10:00 +0000

    display

---
+++ GestureProject/src/ie/gmit/sw/Display/Display.java
@@ -0,0 +1,47 @@
+package ie.gmit.sw.Display;
+
+import java.awt.Canvas;
+
+public class Display extends Canvas implements Runnable{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	//displays and where the program first starts 
+	public static void main(String[] args) {
+		
+	}
+
+	//implement runnable 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+//	public Display() {
+//		//this is the title of the jframe 
+//		super("GUI Project");
+//		// if the x is pressed then JFrame exits 
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        
+//        //the layout
+//        setLayout(null);
+//        // the size of the canvas 
+//        setSize(770,652);
+//        
+//        //displays the canvas
+//        setResizable(false);
+//        setVisible(true);
+//	}
+//
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+//	public static void main(String[]args) throws IOException, FontFormatException{
+//        new Display();
+//    }
+}
---
+++ GestureProject/src/ie/gmit/sw/ShootBullet.java
@@ -0,0 +1,12 @@
+package ie.gmit.sw;
+
+public class ShootBullet {
+	
+	// this will be the direction in which you will shoot 
+	// either up or down
+	public static final int UP = -1; 
+    public static final int DOWN = 1;
+    
+    
+
+}
