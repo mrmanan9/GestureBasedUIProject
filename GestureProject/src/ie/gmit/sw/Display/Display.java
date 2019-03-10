58d33fc935f679251dc9b5c0597f1dd46088acc7
Nakster <nakster58@gmail.com>
2019-03-10 16:03:09 +0000

    display black backgroud

--- GestureProject/src/ie/gmit/sw/Display/Display.java
+++ GestureProject/src/ie/gmit/sw/Display/Display.java
@@ -1,6 +1,9 @@
 package ie.gmit.sw.Display;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -36,7 +39,7 @@ public class Display extends Canvas implements Runnable{
 		frame.setVisible(true);
 
 		//the layout
-		frame.setLayout(null);
+		//frame.setLayout(null);
 		// the size of the canvas 
 		frame.setSize(770,652);
 		
@@ -45,6 +48,7 @@ public class Display extends Canvas implements Runnable{
 	}
 
 	// shares resources 
+	// run the thread
 	public synchronized void start() {
 		//each time this is run this will share the resources with another thread
 		if(running) {
@@ -57,7 +61,8 @@ public class Display extends Canvas implements Runnable{
 		//start the thread
 		thread.start();
 	}
-
+	
+	//stop the thread 
 	public synchronized void stop() {
 		//stop the thread
 		if(running) {
@@ -82,11 +87,38 @@ public class Display extends Canvas implements Runnable{
 		// after 1000 times
 		int counter = 0;
 		
+		//create createBufferStrategy to start drawing the game 
+		this.createBufferStrategy(3);
+		BufferStrategy bs = this.getBufferStrategy();
+
 		while(running) {
+			
+			// call the draw method 
+			draw(bs);
+			
 			counter++;
 			if(counter == 1000) {
 				System.out.println("The Game is Running");
 			}
 		}
+		
 	}// end of run method 
+	
+	public void draw(BufferStrategy bs) {
+		// this is a good way to use buffered image so there isnt
+		// a flicker also it doesnt take too many resources
+		do {
+			do {
+				
+				// this here sets the background color to black
+				Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+				g.setColor(Color.BLACK);
+				g.fillRect(0, 0, 820,710);
+				
+				
+			} while (bs.contentsRestored());
+			//shows the current thing that was drawn 
+			bs.show();
+		}  while (bs.contentsLost());	
+	}
 }
