8e41efce35ed0aa98b8559d4299fa9241e73e9f8
Nakster <nakster58@gmail.com>
2019-04-03 19:05:16 +0100

    adding more methods

--- GestureProject/src/ie/gmit/sw/Sprites/Sprite.java
+++ GestureProject/src/ie/gmit/sw/Sprites/Sprite.java
@@ -4,6 +4,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
+
 public class Sprite {
 	
 	// variables 
@@ -16,6 +17,8 @@ public class Sprite {
 	
 	//buffered image
 	private ArrayList<BufferedImage> sprites = new ArrayList<BufferedImage>();
+	//current sprite 
+	private byte currentSprite;
 	
 	// x and y position of the sprite on the screen
 	// speed var is for the speed on the screen
@@ -27,7 +30,8 @@ public class Sprite {
 	
 	//draw 
 	public void draw(Graphics2D g) {
-		
+		//draw sprite 
+		g.drawImage(sprites.get(currentSprite), (int) getXpos(), (int) getYpos(), null);
 	}
 	//update 
 	public void update(double delta) {
@@ -43,5 +47,54 @@ public class Sprite {
 	public void addSprite(BufferedImage spriteMap, int xPos, int yPos,int width, int height) {
 		sprites.add(spriteMap.getSubimage(xPos, yPos, width, height));
 	}
+	
+	
+	// stop the animation
+	public void stopAnimation() {
+		loop = false;
+		play = false;
+	}
+	
+	// reset the animation
+	public void resetSprite() {
+		loop = false;
+		play = false;
+		currentSprite = 0;
+	}
+	
+	private void loopAnimation() {
+		
+	}
+	
+	private void playAnimation() {
+	
+	}
+	
+	public boolean isSpriteAnimDestroyed() {
+		if (sprites == null)
+			return true;
+
+		return false;
+	}
+	
+	//getteres and setters 
+	public double getXpos() {
+		return xpos;
+	}
+
+	public void setXpos(double xpos) {
+		this.xpos = xpos;
+	}
+
+	public double getYpos() {
+		return ypos;
+	}
+
+	public void setYpos(double ypos) {
+		this.ypos = ypos;
+	}
+	
+	
+	
 
 }
