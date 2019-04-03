61115baa962589aceeab335537770daaafdf4640
Nakster <nakster58@gmail.com>
2019-04-04 00:17:01 +0100

    draws cannon image

--- GestureProject/src/ie/gmit/sw/Screen/GameScreen.java
+++ GestureProject/src/ie/gmit/sw/Screen/GameScreen.java
@@ -3,26 +3,37 @@ package ie.gmit.sw.Screen;
 import java.awt.Canvas;
 import java.awt.Graphics2D;
 
+import ie.gmit.sw.Sprites.Player;
 import ie.gmit.sw.StateMachine.Statator;
 
 public class GameScreen implements Statator{
+	
+	//player cannon
+	private Player cannon;
+	
+	public GameScreen(){
+		// send the constructor the positions 
+		cannon = new Player(150,150 , 50, 50);
+	}
 
 	@Override
 	public void update(double delta) {
 		// TODO Auto-generated method stub
-		
+		cannon.update(delta);
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
 		// TODO Auto-generated method stub
-		
+		// draw the cannon 
+		cannon.draw(g);
 	}
 
 	@Override
 	public void init(Canvas canvas) {
+		// add the player listener to the canvas 
 		// TODO Auto-generated method stub
-		
+		canvas.addKeyListener(cannon);
 	}
 
 }
--- GestureProject/src/ie/gmit/sw/Sprites/Player.java
+++ GestureProject/src/ie/gmit/sw/Sprites/Player.java
@@ -1,13 +1,52 @@
 package ie.gmit.sw.Sprites;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 public class Player implements KeyListener{
 	
+	// player sprite 
+	private BufferedImage player;
 	
+	//variables
+	private double xpos, ypos;
+	private int width, height;
+	
+	//rectangle
+	private Rectangle rect;
+	
+	//constructor 
+	public Player(double xpos, double ypos, int width, int height) {
+		this.xpos = xpos;
+		this.ypos = ypos;
+		this.height = height;
+		this.width = width;
+		
+		//make a new rectangle 
+		rect = new Rectangle((int)xpos, (int)ypos, width, height);
+		try {
+			//open the cannon image 
+			player = ImageIO.read(new File("./Images/cannon.png"));
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	//draw the image here 
 	public void draw(Graphics2D g) {
+		// draw cannon //at the positions given
+		g.drawImage(player,(int) xpos,(int) ypos, width, height, null);
+	}
+	
+	public void update(double delta){
 		
 	}
 
